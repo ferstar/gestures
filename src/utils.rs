744c18f92cef53354bf37d4e29e5bdc65f5b696a
Zhang Jianfei <zhangjianfei3@gmail.com>
2023-02-16 02:21:00 +0800

    fix: Wait for the child process to exit
    https://doc.rust-lang.org/std/process/struct.Child.html#warning

--- src/utils.rs
+++ src/utils.rs
@@ -3,15 +3,16 @@ use regex::Regex;
 use std::process::Command;
 
 pub fn exec_command_from_string(args: &str, dx: f64, dy: f64, scale: f64) -> Result<()> {
-    let rx = Regex::new(r"[^\\]\$delta_x")?;
-    let ry = Regex::new(r"[^\\]\$delta_y")?;
-    let rs = Regex::new(r"[^\\]\$scale")?;
-    let args = ry.replace_all(args, format!(" {} ", dy));
-    let args = rx.replace_all(&args, format!(" {} ", dx));
-    let args = rs.replace_all(&args, format!(" {} ", scale));
     if !&args.is_empty() {
         log::debug!("{:?}", &args);
-        Command::new("sh").arg("-c").arg(&*args).spawn()?;
+        let rx = Regex::new(r"[^\\]\$delta_x")?;
+        let ry = Regex::new(r"[^\\]\$delta_y")?;
+        let rs = Regex::new(r"[^\\]\$scale")?;
+        let args = ry.replace_all(args, format!(" {} ", dy));
+        let args = rx.replace_all(&args, format!(" {} ", dx));
+        let args = rs.replace_all(&args, format!(" {} ", scale));
+        let mut child = Command::new("sh").arg("-c").arg(&*args).spawn()?;
+        child.wait()?;
     }
     Ok(())
 }
